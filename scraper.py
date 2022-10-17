76594faf9a29dff0921e941b630ce38eb95e33d6
Edward Chen <echen333us@gmail.com>
2022-10-16 21:15:09 -0700

    Initial commit

---
+++ scraper.py
@@ -0,0 +1,50 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://mitadmissions.org/blogs/page/"
+PAGE_LIMIT = 10
+SORT_BY = "date" # date, title, comments, responses
+
+blogs = []
+blog_links = []
+outside_links = []
+num_comments = []
+num_responses = []
+
+for i in range(1, PAGE_LIMIT + 1):
+    page = requests.get(URL + str(i))
+
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    results = soup.find_all("a", {"class": "post-tease__h__link"})
+
+    for result in results:
+        blogs.append(result.get_text())
+        blog_links.append(result["href"])
+
+print(blogs)
+print(blog_links)
+
+for i in range(len(blog_links)):
+    page = requests.get(blog_links[i])
+
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    results = soup.find_all("a")
+
+    for result in results:
+        outside_links.append(result["href"])
+
+outside_links = list(set(outside_links))
+outside_links.sort()
+
+f = open("blogs.txt", "w")
+for i in range(len(blogs)):
+    f.write(blogs[i] + "\n")
+    f.write(blog_links[i])
+f.close()
+
+f = open("outside_links.txt", "w")
+for link in outside_links:
+    f.write(link + "\n")
+f.close()
